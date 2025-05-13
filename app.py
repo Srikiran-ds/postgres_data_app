c7ea04cb994eebc60cf62a6f493e58429960e507
Srikiran Goud Chagapuram <64151178+Srikiran-ds@users.noreply.github.com>
2025-05-14 00:20:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,9 @@
 import streamlit as st
-import psycopg2
-from psycopg2 import sql
+from streamlit_gsheets import GSheetsConnection
 
-# Database connection
-def create_connection():
-    try:
-        conn = psycopg2.connect(
-            host="pg-20229fa0-kiran-b069.h.aivencloud.com",  # Update with your PostgreSQL host
-            database="defaultdb",  # Update with your database name
-            user="avnadmin",  # Update with your username
-            password="AVNS_MDKyGWGnf00wVG_AwCl"  # Update with your password
-        )
-        return conn
-    except Exception as e:
-        st.error(f"Error connecting to the database: {e}")
-        return None
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+st.write("connection success")
 
-# Insert data into the database
-def insert_data(conn, table, data):
-    try:
-        with conn.cursor() as cursor:
-            columns = data.keys()
-            values = [data[column] for column in columns]
-            insert_query = sql.SQL("INSERT INTO {table} ({fields}) VALUES ({values})").format(
-                table=sql.Identifier(table),
-                fields=sql.SQL(', ').join(map(sql.Identifier, columns)),
-                values=sql.SQL(', ').join(sql.Placeholder() * len(values))
-            )
-            cursor.execute(insert_query, values)
-            conn.commit()
-            st.success("Data inserted successfully!")
-    except Exception as e:
-        st.error(f"Error inserting data: {e}")
-
-# Streamlit app
-st.title("PostgreSQL Data Entry App")
-
-conn = create_connection()
-
-if conn:
-    table_name = st.text_input("Enter the table name:")
-    if table_name:
-        st.header(f"Insert Data into {table_name}")
-        input_data = {}
-        column_name = st.text_input("Enter column name:")
-        column_value = st.text_input("Enter column value:")
-        if column_name and column_value:
-            input_data[column_name] = column_value
-        if st.button("Submit"):
-            insert_data(conn, table_name, input_data)
-
-conn.close()
+df = conn.read(sheet="Expenses", ttl="10m")
+st.write(df)
