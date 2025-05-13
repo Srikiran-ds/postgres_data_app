1da5aa6f8999de84b5720aa0e0e34527ac4dbafe
Srikiran Goud Chagapuram <64151178+Srikiran-ds@users.noreply.github.com>
2025-05-14 00:51:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import pandas as pd
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-st.write("connection success")
+tab1, tab2, tab3 = st.tabs(["Update", "Read", "Analysis"])
+tab2.write("connection success")
 df = conn.read(worksheet="sample", ttl="0.5m")
-st.write(df)
+tab2.write(df)
 
 # Add the new vendor data to the existing data
-name = st.text_input(label="Name")
-age = st.text_input(label="age")
-submit_button = st.button(label="Submit Details")
+name = tab1.text_input(label="Name")
+age = tab1.text_input(label="age")
+submit_button = tab1.button(label="Submit Details")
 if submit_button:
     new_data = pd.DataFrame(
                 [
@@ -25,5 +26,5 @@ if submit_button:
     # Update Google Sheets with the new vendor data
     conn.update(worksheet="sample", data=updated_df)
     st.success("Vendor details successfully submitted!")
-    df = conn.read(worksheet="sample", ttl="0.5m")
-    st.write(df)
+    #df = conn.read(worksheet="sample", ttl="0.5m")
+    #st.write(df)
