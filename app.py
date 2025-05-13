115ae7d76a2b374897183c7aa2e51da983cbc34d
Srikiran Goud Chagapuram <64151178+Srikiran-ds@users.noreply.github.com>
2025-05-14 00:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-st.write("connection success")
+st.write("connection success or what")
 
 df = conn.read(sheet="sample", ttl="10m")
 st.write(df)
